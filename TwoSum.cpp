9d807bf95246dc35b5cbd6142368c28dd69717f7
Monstar <652141046@qq.com>
2017-09-04 22:14:28 +0800

    1

---
+++ TwoSum.cpp
@@ -0,0 +1,15 @@
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> sum(2, 0);
+        for (auto i = 0; i != (nums.size() - 1); ++i)
+            for (auto j = i + 1; j != nums.size(); ++j)
+                if (nums[i] + nums[j] == target)
+                    sum = {i, j};
+        return sum;
+    }
+};
+*/
+
+
